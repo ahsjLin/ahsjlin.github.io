8db60c70c173560c16ce7cc2c63ae51ad8eec70d
tzyrrxr <sjlin0827@outlook.com>
2021-10-13 22:09:26 +0800

    ` `

---
+++ docs/leetcode/string/easy/Length of Last Word/length_of_last_word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	int lengthOfLastWord(string s) {
+		bool doStartCounting=0;
+		int counter=0;
+		for(int i=s.size()-1; i>=0; i--)
+		{
+			// ascii(' ') : 32
+			if(int(s[i])!=32){
+				doStartCounting = 1;
+				counter++;
+			}
+			else if(int(s[i])==32 && doStartCounting) break;
+		}
+		return counter;
+	}
+};
