52be64755ad37e22edfcbcf1b9f0d46623c5d379
tzyrrxr <sjlin0827@outlook.com>
2021-10-18 00:56:01 +0800

    Mon 10/18/2021  0:56:00.95

--- docs/leetcode/array/easy/118_Pascal_s_Triangle/118_Pascal_s_Triangle.cpp
+++ docs/leetcode/array/easy/118_Pascal_s_Triangle/118_Pascal_s_Triangle.cpp
@@ -4,20 +4,20 @@ public:
 		// return vector
 		vector<vector<int>> rev;
 		// temp row vector
-		vector<int> temp_v;
+		vector<int> temp_row;
 
 		// right index
 		int r;
 		for(int i=0; i<numRows; i++){
 			r=i;
 			// default row size and put them all 1.
-			temp_v.resize(i+1, 1);
+			temp_row.resize(i+1, 1);
 			for(
 				int j=1;
 				j<r;
-				temp_v[j]=rev[i-1][j-1]+rev[i-1][j], j++
+				temp_row[j]=rev[i-1][j-1]+rev[i-1][j], j++
 			   );
-			rev.push_back(temp_v);
+			rev.push_back(temp_row);
 		}
 		return rev;
 	}
